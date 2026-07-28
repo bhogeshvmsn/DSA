2b7fe2ca34672a296825f78de8d994d59973d79d
bhogeshvmsn <115815015+bhogeshvmsn@users.noreply.github.com>
2026-07-28 23:10:55 +0530

    Time: 499 ms (14.29%) | Memory: 22 MB (6.91%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        n=len(s)
+
+        if n%2 ==1:
+            h1=sorted(s[:n//2])
+            h2=sorted(s[n//2+1:],reverse=True)
+            return "".join(h1+[s[n//2]]+h2)
+        else:
+            h1=sorted(s[:n//2])
+            h2=sorted(s[n//2:],reverse=True)
+            return "".join(h1+h2)
+            
